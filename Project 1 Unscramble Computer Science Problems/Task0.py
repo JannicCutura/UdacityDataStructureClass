6741066dfa572020df0d31dde4a28eb7ddfaaa0d
Jannic Cutura <Janniccutura@gmail.com>
2020-03-28 12:19:16 +0100

    wrote solution

--- Project 1 Unscramble Computer Science Problems/Task0.py
+++ Project 1 Unscramble Computer Science Problems/Task0.py
@@ -3,11 +3,11 @@ Read file into texts and calls.
 It's ok if you don't understand how to read files.
 """
 import csv
-with open('texts.csv', 'r') as f:
+with open('Project 1 Unscramble Computer Science Problems\\texts.csv', 'r') as f:
     reader = csv.reader(f)
     texts = list(reader)
 
-with open('calls.csv', 'r') as f:
+with open('Project 1 Unscramble Computer Science Problems\\calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
 
@@ -19,4 +19,14 @@ Print messages:
 "First record of texts, <incoming number> texts <answering number> at time <time>"
 "Last record of calls, <incoming number> calls <answering number> at time <time>, lasting <during> seconds"
 """
+
+print("First record of texts, {} texts {} at time {}".format(texts[0][0], texts[0][1], texts[0][2]))
+print("Last record of calls, {} calls {} at time {}, lasting {}  seconds".format(calls[-1][0], calls[-1][1], calls[-1][2], calls[-1][3]))
+
+
+
+
+
+
+
 
