f81e5b8d352c59b987ad58091351966f9383f321
Jannic Cutura <Janniccutura@gmail.com>
2020-04-03 17:46:37 +0200

    added solutions

--- Project 1 Show me the data structures/problem_1.py
+++ Project 1 Show me the data structures/problem_1.py
@@ -1,205 +1,181 @@
-# solution partially adapted from: https://knowledge.udacity.com/questions/93168
-## from the forum
+"""
+I went in detail over this solution  https://knowledge.udacity.com/questions/93168 but then built my own code from scratch
+I remember the basic structure was:
+ - DoubleNode class to build a doubleLinkedList
+ - Use a dictionary to track where a given key in the list is, so you can remove it in O(1) and do not have to loop
+"""
+
+
 class DoubleNode:
     def __init__(self, key, value):
         self.key = key
         self.value = value
-        self.prev = None
         self.next = None
-
-    def removeLinks(self):
-        if self.prev is not None:
-            self.prev.next = self.next
-        if self.next is not None:
-            self.next.prev = self.prev
         self.prev = None
-        self.next = None
 
-    def __str__(self):
-        return str(self.value)
+# lets first test the doublenode class a bit
+node1 = DoubleNode(1,1)
+node2 = DoubleNode(2,3)
+node1.key
+node2.value
+#ok works as expected
 
-    def print_info(self):
-        print("The key is {} and the value is {}, the next node is {} and the previous node is {}".format(self.key,self.value, self.next, self.prev))
 
-class DoublyLinkedList:
+
+class DoubleLinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
 
-    def prepend(self, node):
-        # if the most recent element is used again, don't do anything
-        if self.head == node:
-            return
+    def preprend(self, node):
 
-        # if the list is empty, add new tail and head
-        elif self.head is None:
+        # if llist is empty
+        if self.head is None:
             self.head = node
             self.tail = node
 
-        # if the list has only one element, (head == tail)
+        # if head is the node element, just ignore it
+        elif self.head == node:
+            return
+
+        # if the list has one element, then move the tail forward
         elif self.head == self.tail:
             self.tail.prev = node
             self.head = node
             self.head.next = self.tail
 
-        # any other case, i.e. at least two elements
-        else:
-            # if the tail is the same node, remove it
-            if self.tail == node:
-                self.removeTail()
 
-            node.removeLinks()
-            self.head.prev = node
+
+        # if the list has multiple elements
+        else:
+            if self.tail == node:
+                self.remove_tail()
+            # "take one out in the middle"
+            if node.prev is not None:
+                node.prev.next = node.next
+            if node.next is not None:
+                node.next.prev = node.prev
+
+            # "and add it in the front"
             node.next = self.head
+            self.head.prev = node
             self.head = node
 
-    def removeTail(self):
+
+    def remove_tail(self):
         if self.tail is None:
             return
+
         if self.tail == self.head:
             self.head = None
             self.tail = None
             return
-        self.tail = self.tail.prev
-        self.tail.next = None
+
+        else:
+            new_tail = self.tail.prev
+            new_tail.next = None
+            self.tail = new_tail
 
     def __str__(self):
-        to_return = 'Most Recent '
-        curr = self.head
-        while curr != None:
-            to_return += str(curr)
-            if curr.next != None:
-                to_return += ' <--> '
-            curr = curr.next
-        return to_return + ' Least Recent'
+        if self.head is None:
+            return "List is empty"
+        else:
+            cur_node = self.head
+            to_display = "Last added  -->  "
+            to_display = to_display + str(cur_node.value)
+            while cur_node.next:
+                to_display = to_display+  " | " + str(cur_node.next.value)
+                cur_node = cur_node.next
+            to_display = to_display  + " <-- next to drop "
+
+        return to_display
+
+
+llist = DoubleLinkedList()
+llist.preprend(node1)
+llist.preprend(node2)
+print(llist)
+#ok works as expected
+del node1, node2, llist
 
 
 class LRU_Cache(object):
+
     def __init__(self, capacity):
         # Initialize class variables
+        self.num_elements = 0
         self.capacity = capacity
-        self.hash = {}
-        self.currentSize = 0
-        self.listOfMostRecent = DoublyLinkedList()
+        self.hash = dict()
+        self.llist = DoubleLinkedList()
 
     def get(self, key):
-        # Retrieve item from provided key. Return -1 if nonexistent.
         if key not in self.hash:
             return -1
-        # Update the most recent item
-        self.updateMostRecent(self.hash[key])
+
+        self.llist.preprend(self.hash[key])
         return self.hash[key].value
 
+
     def set(self, key, value):
-        # Set the value if the key is not present in the cache. If the cache is at capacity remove the oldest item.
-        if key not in self.hash:
-            if self.currentSize == self.capacity:
-                self.removeLeastRecent()
-            else:
-                self.currentSize += 1
-            self.hash[key] = DoubleNode(key, value)
+        if key in self.hash:
+            self.llist.preprend(self.hash[key])
+            self.hash[key] = value ## in case a different value is associated with the key
+
         else:
-            self.replaceKey(key, value)
-        self.updateMostRecent(self.hash[key])
-        # note that self.hash[key] is a node object!
+            if self.num_elements == self.capacity:
+                ## here we need to remove the tail
+                del self.hash[self.llist.tail.key]
+                self.llist.remove_tail()
+            else:
+                ## here we can just add
+                self.num_elements +=1
 
-    def removeLeastRecent(self):
-        keyToRemove = self.listOfMostRecent.tail.key
-        self.listOfMostRecent.removeTail()
-        del self.hash[keyToRemove]
+            self.hash.update({key:DoubleNode(key, value)})
 
-    def updateMostRecent(self, node):
-        self.listOfMostRecent.prepend(node)
-
-    def replaceKey(self, key, value):
-        if key not in self.hash:
-            raise Exception('The key to be replaced is not in the cache - the action cannot be completed.')
-        self.hash[key].value = value
+            self.llist.preprend(self.hash[key])
 
     def __repr__(self):
-        return str(self.listOfMostRecent)
+        return str(self.llist)
 
     def get_hash(self):
         return {k: str(v) for k, v in self.hash.items()}
 
-
 our_cache = LRU_Cache(5)
+
 our_cache.set(1, 1);
 our_cache.set(2, 2);
-print()
-print('Cache after adding 1, 2, 3 and 4 in that order: ')
-print(our_cache)
-print('Hash status:', our_cache.get_hash())
-print()
-print('Print the our_cache.hash to see the actual objects here:')
-print(our_cache.hash)
-print('Okay, so we have a dcitionary where key 1 points to a node and so does key 2. Lets inspects them.')
-print('First, lets look at the node where key is 1')
-print('Print its key, value, next and prev')
-our_cache.hash[1].key
-our_cache.hash[1].value
-our_cache.hash[1].next
-our_cache.hash[1].prev
-print('the interesting thing here is that our_cache.hash[1].prev again is a node. lets test whether it is the next one:')
-our_cache.hash[1].prev == our_cache.hash[2]
-print('Indeed! This is extremely useful, since by only providing the new element for the cache we can get that'
-      'element from the LinkedList listOfMostRecent. This allows to delete such an element by replacing the prev and next '
-      'links.')
-
-
-
-
 our_cache.set(3, 3);
 our_cache.set(4, 4);
-print()
-print('Cache after adding 1, 2, 3 and 4 in that order: ')
-print(our_cache)
-print('Hash status:', our_cache.get_hash())
-print()
-print('our_cache.get(1) returns: ', our_cache.get(1))
-print('Cache after getting 1.')
-print(our_cache)
-print('Hash status:', our_cache.get_hash())
-print()
-print('our_cache.get(2) returns: ', our_cache.get(2))
-print('Cache after getting 2.')
-print(our_cache)
-print('Hash status:', our_cache.get_hash())
-print()
-print('our_cache.get(9) returns: ', our_cache.get(9))
-print('Cache after getting 9.')
-print(our_cache)
-print('Hash status:', our_cache.get_hash())
 
-# Let's add some more items to see if this works right
+our_cache.get(1)       # returns 1
+our_cache.get(2)       # returns 2
+our_cache.get(9)      # returns -1 because 9 is not present in the cache
+
 our_cache.set(5, 5)
-print('Cache after adding 5:\n', our_cache)
-print('Hash status:', our_cache.get_hash())
-print(our_cache)
-print()
 our_cache.set(6, 6)
-print('Cache after adding 6:\n', our_cache)
-print('Hash status:', our_cache.get_hash())
-print()
-our_cache.set(7, 7)
-print('Cache after adding 7:\n', our_cache)
-print('Hash status:', our_cache.get_hash())
 
-print('our_cache.get(3) returns: ', our_cache.get(3))
-print('Cache after adding get(3):\n', our_cache)
-print('Hash status:', our_cache.get_hash())
+our_cache.get(3)      # returns -1 because the cache reached it's capacity and 3 was the least recently used entry
 
-our_cache.set(5, 5)
-print('Cache after adding 5:\n', our_cache)
-print('Hash status:', our_cache.get_hash())
 
-our_cache.set(5, 5)
 
-## some additional print outs for my understanding
-our_cache.hash
-for key, value in our_cache.hash.items():
-    print(key)
-    print(value.print_info())
+## okay these all work as expected, here are some more for me:
+
+print("This is the cache:")
+print(our_cache)
+print("and here we see the cash with the pointers to where the Nodes are in memory (0x000121245... notation)")
+print(our_cache.hash)
+print("lets dig a bit. Get the first Node")
+node1 = our_cache.hash[1]
+print("Print, key, value and the prev attribute:")
+node1.key
+node1.value
+print(node1.prev)
+print("if the implemntation is correct, our_cache.hash[2]  should be  our_cache.hash[1].prev. Is it?")
+node1.prev == our_cache.hash[2]
+print("and vice verse, our_cache.hash[2].next  should be  our_cache.hash[1]. Is it?")
+node1 == our_cache.hash[2].next
+
+
 
 
 
