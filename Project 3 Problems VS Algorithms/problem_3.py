5d2cdc22a1033379540affac2cd6b05a646eb7b1
Jannic Cutura <Janniccutura@gmail.com>
2020-04-13 13:50:19 +0200

    added solutions

--- Project 3 Problems VS Algorithms/problem_3.py
+++ Project 3 Problems VS Algorithms/problem_3.py
@@ -10,16 +10,61 @@ The expected answer would be [531, 42]. Another expected answer can be [542, 31]
 
 Here is some boilerplate code and test cases to start with:
 """
-def rearrange_digits(input_list):
-    """
-    Rearrange Array Elements so as to form two number such that their sum is maximum.
 
-    Args:
-       input_list(list): Input List
-    Returns:
-       (int),(int): Two maximum sums
-    """
-    pass
+
+def sort_a_little_bit(items, begin_index, end_index):
+    left_index = begin_index
+    pivot_index = end_index
+    pivot_value = items[pivot_index]
+
+    while (pivot_index != left_index):
+
+        item = items[left_index]
+
+        if item <= pivot_value:
+            left_index += 1
+            continue
+
+        items[left_index] = items[pivot_index - 1]
+        items[pivot_index - 1] = pivot_value
+        items[pivot_index] = item
+        pivot_index -= 1
+
+    return pivot_index
+
+def sort_all(items, begin_index, end_index):
+    if end_index <= begin_index:
+        return
+
+    pivot_index = sort_a_little_bit(items, begin_index, end_index)
+    sort_all(items, begin_index, pivot_index - 1)
+    sort_all(items, pivot_index + 1, end_index)
+
+def quicksort(items):
+    sort_all(items, 0, len(items) - 1)
+
+
+
+
+def rearrange_digits(input_list):
+    if len(input_list) < 2:
+        print("Input at least two numbers!")
+        return
+
+    quicksort(input_list) # this takes O(n log(n)) time
+    larger_number = ""
+    smaller_number = ""
+    length = len(input_list)-1
+
+
+    for i in range(0,length+1): ## this takes O(n)
+        if i % 2 == 1:
+             smaller_number = smaller_number + str(input_list[length - i])
+        else:
+            larger_number = larger_number +str(input_list[length - i])
+    return [int(larger_number), int(smaller_number)]
+
+
 
 def test_function(test_case):
     output = rearrange_digits(test_case[0])
@@ -29,5 +74,16 @@ def test_function(test_case):
     else:
         print("Fail")
 
+
+# test case 1: From the problem set
 test_function([[1, 2, 3, 4, 5], [542, 31]])
-test_case = [[4, 6, 2, 5, 9, 8], [964, 852]]
+test_function([[4, 6, 2, 5, 9, 8], [964, 852]])
+
+# test case 2: Empty input
+rearrange_digits([])
+# Input at least two numbers!
+
+
+# test case 3: Trivial input
+test_function([[1, 1], [1, 1]])
+test_function([[0, 0], [0, 0]])
--- Project 3 Problems VS Algorithms/problem_7.py
+++ Project 3 Problems VS Algorithms/problem_7.py
@@ -3,52 +3,86 @@ HTTPRouter using a Trie
 
 For this exercise we are going to implement an HTTPRouter like you would find in a typical web server using the Trie data structure we learned previously.
 
-There are many different implementations of HTTP Routers such as regular expressions or simple string matching, but the Trie is an excellent and very efficient data structure for this purpose.
+There are many different implementations of HTTP Routers such as regular expressions or simple string matching,
+but the Trie is an excellent and very efficient data structure for this purpose.
 
-The purpose of an HTTP Router is to take a URL path like "/", "/about", or "/blog/2019-01-15/my-awesome-blog-post" and figure out what content to return. In a dynamic web server, the content will often come from a block of code called a handler.
+The purpose of an HTTP Router is to take a URL path like "/", "/about", or "/blog/2019-01-15/my-awesome-blog-post" and figure out what content to return.
+In a dynamic web server, the content will often come from a block of code called a handler.
 
-First we need to implement a slightly different Trie than the one we used for autocomplete. Instead of simple words the Trie will contain a part of the http path at each node, building from the root node /
+First we need to implement a slightly different Trie than the one we used for autocomplete.
+Instead of simple words the Trie will contain a part of the http path at each node, building from the root node /
 
-In addition to a path though, we need to know which function will handle the http request. In a real router we would probably pass an instance of a class like Python's SimpleHTTPRequestHandler which would be responsible for handling requests to that path. For the sake of simplicity we will just use a string that we can print out to ensure we got the right handler
+In addition to a path though, we need to know which function will handle the http request.
+In a real router we would probably pass an instance of a class like Python's SimpleHTTPRequestHandler
+which would be responsible for handling requests to that path.
+For the sake of simplicity we will just use a string that we can print out to ensure we got the right handler
 
-We could split the path into letters similar to how we did the autocomplete Trie, but this would result in a Trie with a very large number of nodes and lengthy traversals if we have a lot of pages on our site. A more sensible way to split things would be on the parts of the path that are separated by slashes ("/"). A Trie with a single path entry of: "/about/me" would look like:
+We could split the path into letters similar to how we did the autocomplete Trie,
+but this would result in a Trie with a very large number of nodes and lengthy traversals if we have a lot of pages on our site.
+A more sensible way to split things would be on the parts of the path that are separated by slashes ("/").
+A Trie with a single path entry of: "/about/me" would look like:
 
 (root, None) -> ("about", None) -> ("me", "About Me handler")
 
-We can also simplify our RouteTrie a bit by excluding the suffixes method and the endOfWord property on RouteTrieNodes. We really just need to insert and find nodes, and if a RouteTrieNode is not a leaf node, it won't have a handler which is fine.
+We can also simplify our RouteTrie a bit by excluding the suffixes method and the endOfWord property on RouteTrieNodes.
+We really just need to insert and find nodes, and if a RouteTrieNode is not a leaf node,
+it won't have a handler which is fine.
 
 """
+
+
 
 # A RouteTrie will store our routes and their associated handlers
 class RouteTrie:
-    def __init__(self, ...):
-        # Initialize the trie with an root node and a handler, this is the root path or home page node
+    def __init__(self):
+        self.root = RouteTrieNode()
 
-    def insert(self, ...):
+
+    def insert(self, newpage):
         # Similar to our previous example you will want to recursively add nodes
         # Make sure you assign the handler to only the leaf (deepest) node of this path
+        newpage = newpage.split("/")
+        current_node = self.root
 
-    def find(self, ...):
+        for directory in newpage:
+            if directory not in current_node.children:
+                current_node.insert(directory)
+            current_node = current_node.children[directory]
+
+        current_node.handler = newpage[-1] + " handler"
+
+    def find(self, url):
         # Starting at the root, navigate the Trie to find a match for this path
         # Return the handler for a match, or None for no match
+        pass
+
 
 # A RouteTrieNode will be similar to our autocomplete TrieNode... with one additional element, a handler.
 class RouteTrieNode:
-    def __init__(self, ...):
-        # Initialize the node with children as before, plus a handler
+    def __init__(self):
+        self.children = {}
+        self.handler = None
 
-    def insert(self, ...):
-        # Insert the node as before
-        
-        
+    def insert(self, page):
+        self.children[page] = RouteTrieNode()
+
+
+## some initial testing:
+mypage = RouteTrie()
+mypage.insert("about")
+mypage.insert("about/me")
 """
-Next we need to implement the actual Router. The router will initialize itself with a RouteTrie for holding routes and associated handlers. It should also support adding a handler by path and looking up a handler by path. All of these operations will be delegated to the RouteTrie.
+Next we need to implement the actual Router.
+ The router will initialize itself with a RouteTrie for holding routes and associated handlers.
+ It should also support adding a handler by path and looking up a handler by path.
+ All of these operations will be delegated to the RouteTrie.
 
 Hint: the RouteTrie stores handlers under path parts, so remember to split your path around the '/' character
 
 Bonus Points: Add a not found handler to your Router which is returned whenever a path is not found in the Trie.
 
-More Bonus Points: Handle trailing slashes! A request for '/about' or '/about/' are probably looking for the same page. Requests for '' or '/' are probably looking for the root handler. Handle these edge cases in your Router.
+More Bonus Points: Handle trailing slashes! A request for '/about' or '/about/' are probably looking for the same page.
+ Requests for '' or '/' are probably looking for the root handler. Handle these edge cases in your Router.
 
 """
 
@@ -57,6 +91,7 @@ class Router:
     def __init__(self, ...):
         # Create a new RouteTrie for holding our routes
         # You could also add a handler for 404 page not found responses as well!
+        self.RouteTrie = RouteTrie()
 
     def add_handler(self, ...):
         # Add a handler for a path
